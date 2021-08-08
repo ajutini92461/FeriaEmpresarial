d6baafed3f1d5bd387f7f319a83cedf46409d7da
ajutini92461 <68123509+ajutini92461@users.noreply.github.com>
2021-08-07 22:17:45 -0500

    Add files via upload

---
+++ feriaempresarial/mundo/Expositores.java
@@ -0,0 +1,20 @@
+package universidadean.feriaempresarial.mundo;
+
+public class Expositores {
+
+    private String Zona;
+    private int NExpositores;
+
+    public void setNExpositores(int nExpositores) {
+        NExpositores = nExpositores;
+    }
+    public void setZona(String zona) {
+        Zona = zona;
+    }
+    public int getNExpositores() {
+        return NExpositores;
+    }
+    public String getZona() {
+        return Zona;
+    }
+}
